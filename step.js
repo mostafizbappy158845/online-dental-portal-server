356962b1183e86f95a1563f30b66176528757f2f
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-01-18 01:29:46 +0600

    server mongodb connected

---
+++ step.js
@@ -0,0 +1,6 @@
+// 1.index.js file make
+// 2.npm i express cors dotenv mongodb
+//3. add 
+    //   "start": "node index.js",
+    //   "start-dev": "nodemon index.js",
+    // on package.json>script
