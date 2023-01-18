1ca168d7306974e24cfc22b492ac1b09bdefe6fb
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-01-19 00:20:36 +0600

    booking-save connect to database

--- index.js
+++ index.js
@@ -18,10 +18,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         const appointmentOptionCollection = client.db('dentalPortal').collection('appointmentOptions');
+        const bookingsCollection = client.db('dentalPortal').collection('bookings');
         app.get('/appointmentOptions', async(req, res) =>{
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
             res.send(options);
+        })
+
+        app.post ('/bookings', async(req, res) =>{
+            const booking =  req.body
+            console.log(booking);
+            const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
         })
     }
     finally{
