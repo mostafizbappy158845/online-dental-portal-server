041d57c62aa6544b9bbcf922adc1f174be796e95
mostafizbappy <mostafizur15-8845@diu.edu.bd>
2023-02-17 23:48:19 +0600

    Load all user on dashboard page

--- index.js
+++ index.js
@@ -98,7 +98,13 @@ async function run(){
                 return res.send({accessToken: token});
             }
             res.status(403).send({accessToken: ''})
-        })
+        });
+
+        app.get ('/users',async(req,res) =>{
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        });
 
         app.post('/users', async(req,res) =>{
             const user = req.body;
